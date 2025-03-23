bc63d25c49083d55f85275376df1e373807dcb56
Carlos Martinez <carlos.martinez.871208@gmail.com>
2025-03-23 17:32:53 -0600

    chore: Std_types.h added to the project.

--- inc/generic/Std_types.h
+++ inc/generic/Std_types.h
@@ -1,18 +1,18 @@
 /*********************************************************************************************************************/
-/*                                                  SOURCE GROUP                                                     */
+/*                                                INCLUDES GROUP                                                     */
 /*********************************************************************************************************************/
 /*                                               OBJECT SPECIFICATION                                                */
 /*********************************************************************************************************************/
 /*!
- * $File: main.c
+ * $File: Std_types.h
  * $Revision: Version 1.0 $
  * $Author: Carlos Martinez $
  * $Date: 2025-03-23 $
  */
 /*********************************************************************************************************************/
 /* DESCRIPTION :                                                                                                     */
-/* main.c:
-           This files is use to initialice microcontroller features.
+/* Std_types.h:
+               Use this template for your header files.
  */
 /*********************************************************************************************************************/
 /* ALL RIGHTS RESERVED                                                                                               */
@@ -21,34 +21,78 @@
 /* not permitted without express written authority. Offenders will be liable                                         */
 /* for damages.                                                                                                      */
 /*********************************************************************************************************************/
-
-/*                                                 Standard libraries                                                */
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+/*                                                       Includes                                                    */
 /*********************************************************************************************************************/
 
-/*                                                   User libraries                                                  */
-/*********************************************************************************************************************/
-#include "Std_types.h"
 /*                                                        Types                                                      */
 /*********************************************************************************************************************/
+/* Unsigned types: */
+/* 32 bits data type. */
+#ifndef uint32_t
+ typedef unsigned int uint32_t;
+#endif
+/* 16 bits data type. */
+#ifndef uint16_t
+ typedef unsigned short int uint16_t;
+#endif
+/* 8 bits data type. */
+#ifndef uint8_t
+ typedef unsigned char uint8_t;
+#endif
 
-/*                                                      Constants                                                    */
+/* Signed types: */
+/* 32 bits data type. */
+#ifndef sint32_t
+ typedef signed int sint32_t;
+#endif
+/* 16 bits data type. */
+#ifndef sint16_t
+ typedef signed short int sint16_t;
+#endif
+/* 8 bits data type. */
+#ifndef sint8_t
+ typedef signed char sint8_t;
+#endif
+
+/* Bool (boolean) type: */
+#ifndef bool
+ typedef enum{false=0u,true} bool;
+#endif
+
+/* Function pointer type definition, points a void function type. */
+#ifndef func_ptr
+ typedef void(*func_ptr)(void);
+#endif
+
+/* IO definitions */
+/* Defines read only permissions. */
+#define __I volatile const
+/* Defines write only permissions. */
+#define __O volatile
+/* Defines read / write permissions. */
+#define __IO volatile
+
+ /*                                                      Constants                                                   */
+/*********************************************************************************************************************/
+/* NULL pointer type definition. */
+#ifndef NULL
+ #define NULL (void*)(0u)
+#endif 
+
+#ifndef EXIT_SUCCESS
+ #define EXIT_SUCCESS true;
+#endif
+
+/*                                                 Exported Variables                                                */
 /*********************************************************************************************************************/
 
-/*                                             Local functions prototypes                                            */
-/*********************************************************************************************************************
-
-/*                                           Local functions implementation                                          */
+/*                                            Exported functions prototypes                                          */
 /*********************************************************************************************************************/
-/* main function called from reset handler. */
-int main (void)
-{
-    while(true)
-    {
-        
-    }
-    return EXIT_SUCCESS;
-}
 
+/*********************************************************************************************************************/
+#endif
 /***************************************************Project Logs*******************************************************
  *|    ID   |     Ticket    |     Date    |                               Description                                 |
  *|---------|---------------|-------------|---------------------------------------------------------------------------|
